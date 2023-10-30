9861ce8bc9285d973fc92655ee05a5654b993451
TonyNguyen <20130215@st.hcmuaf.edu.vn>
2023-10-30 10:44:54 +0700

    update UI

--- CipherApp/src/main/java/screens/HashScreen.java
+++ CipherApp/src/main/java/screens/HashScreen.java
@@ -9,10 +9,7 @@ import javax.swing.border.TitledBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
 import java.io.File;
 
 public class HashScreen extends JFrame {
@@ -29,7 +26,7 @@ public class HashScreen extends JFrame {
         this.absHash = absHash;
 
 
-
+        this.setResizable(false);
         this.setTitle(absHash.name);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         Image icon = Toolkit.getDefaultToolkit().getImage("assets/images/icon.png");
@@ -47,6 +44,9 @@ public class HashScreen extends JFrame {
         //output
         renderOutput(panel);
 
+        setFocusable(true);
+        requestFocus();
+
         this.getContentPane().add(panel);
         this.pack();
         this.setVisible(true);
@@ -56,13 +56,16 @@ public class HashScreen extends JFrame {
     private void renderInput(JPanel panel) {
         inputPanel = new JPanel();
         TitledBorder inputBorder = BorderFactory.createTitledBorder(null, Constants.Description.INPUT, TitledBorder.LEFT, TitledBorder.DEFAULT_POSITION, new Font("Arial", Font.ITALIC, 16), Color.BLACK);
-        inputBorder.setBorder(new LineBorder(Color.BLACK, 1));
+        inputBorder.setBorder(new LineBorder(Color.BLACK, 2));
         inputPanel.setBorder(inputBorder);
         //Text
         textPanel = new JPanel();
-        inputTextArea = new JTextArea();
-        inputTextArea.setPreferredSize(new Dimension(600,200));
-        textPanel.add(inputTextArea);
+        inputTextArea = new JTextArea(12, 40);
+        inputTextArea.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+        inputTextArea.setLineWrap(true);
+        inputTextArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(inputTextArea);
+        textPanel.add(scrollPane);
 
         //event inputtext
         inputTextArea.getDocument().addDocumentListener(new DocumentListener() {
@@ -91,14 +94,16 @@ public class HashScreen extends JFrame {
 
         //File
         filePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        filePanel.setPreferredSize(new Dimension(600,200));
+        filePanel.setPreferredSize(inputTextArea.getPreferredSize());
 
         //label name File
         fileNameLabel = new JLabel("Choose File...");
+        fileNameLabel.setFont(new Font("TimesRoman", Font.PLAIN, 20));
         filePanel.add(fileNameLabel);
 
         //button
         openButton = new JButton("Open File");
+        openButton.setFocusable(false);
         filePanel.add(openButton);
 
         //event button
@@ -129,25 +134,32 @@ public class HashScreen extends JFrame {
     private void renderOutput(JPanel panel) {
         outputPanel = new JPanel();
         TitledBorder outputBorder = BorderFactory.createTitledBorder(null, Constants.Description.OUTPUT, TitledBorder.LEFT, TitledBorder.DEFAULT_POSITION, new Font("Arial", Font.ITALIC, 16), Color.BLACK);
-        outputBorder.setBorder(new LineBorder(Color.BLACK, 1));
+        outputBorder.setBorder(new LineBorder(Color.BLACK, 2));
         outputPanel.setBorder(outputBorder);
         //output
-        outputTextArea = new JTextArea();
-        outputTextArea.setPreferredSize(new Dimension(600,200));
-        outputPanel.add(outputTextArea);
+        outputTextArea = new JTextArea(4, 40);
+        outputTextArea.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+        outputTextArea.setLineWrap(true);
+        outputTextArea.setWrapStyleWord(true);
+        outputTextArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(outputTextArea);
+        outputPanel.add(scrollPane);
 
         panel.add(outputPanel);
     }
 
     private void renderControl(JPanel panel) {
         controlPanel = new JPanel();
-        controlPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        controlPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        Font font = new Font("Serif", Font.BOLD, 25);
 
         JLabel nameLabel = new JLabel("Algorithm :");
+        nameLabel.setFont(font);
         controlPanel.add(nameLabel);
 
         nameCipherLabel = new JLabel(absHash.name);
-        Font font = new Font("Serif", Font.BOLD, 20);
+
         nameCipherLabel.setFont(font);
         controlPanel.add(nameCipherLabel);
 
@@ -155,17 +167,25 @@ public class HashScreen extends JFrame {
         JLabel spaceLabel1 = new JLabel("          ");
         controlPanel.add(spaceLabel1);
 
+        Font fontSelect = new Font("Serif", Font.BOLD, 20);
         //type
-        JLabel typeLabel = new JLabel("Type: ");
+        JLabel typeLabel = new JLabel("Type:");
+        typeLabel.setFont(fontSelect);
         controlPanel.add(typeLabel);
 
         //radioButton Text
         radioButtonText = new JRadioButton("Text");
+        radioButtonText.setFont(fontSelect);
+        radioButtonText.setMnemonic(KeyEvent.VK_T);
+        radioButtonText.setFocusable(false);
         radioButtonText.setSelected(true);
         controlPanel.add(radioButtonText);
 
         //radioButton File
         radioButtonFile = new JRadioButton("File");
+        radioButtonFile.setFont(fontSelect);
+        radioButtonFile.setMnemonic(KeyEvent.VK_F);
+        radioButtonFile.setFocusable(false);
         controlPanel.add(radioButtonFile);
 
         buttonGroup = new ButtonGroup();
--- CipherApp/src/main/java/screens/HomeApp.java
+++ CipherApp/src/main/java/screens/HomeApp.java
@@ -13,6 +13,8 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class HomeApp extends JFrame implements ActionListener{
     JPanel maHoaDoiXungPanel, maHoaBatDoiXungPanel, hashAlgorithmPanel, chuKyDienTuPanel;
@@ -21,16 +23,20 @@ public class HomeApp extends JFrame implements ActionListener{
             md4Button, md5Button, sha1Button, sha256Button, sha512Button, sha3_224Button, ripemd_256Button, crc_32Button, shake_256Button,
             chuKyDienTuButton;
     Dimension btnDimension;
+    Font font;
     public HomeApp(){
+        //exit = click X
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("CipherApp");
-        this.setLayout(new FlowLayout());
+
         this.setResizable(false);
+        this.setTitle("CipherApp_NDCopyright");
+        this.setLayout(new FlowLayout());
 
         Image icon = Toolkit.getDefaultToolkit().getImage("assets/images/icon.png");
         this.setIconImage(icon);
 
-        btnDimension = new Dimension(100, 25);
+        btnDimension = new Dimension(140, 35);
+        font = new Font("Arial", Font.BOLD, 16);
 
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
@@ -43,20 +49,29 @@ public class HomeApp extends JFrame implements ActionListener{
         //Chu ky dien tu
         renderChuKyDienTu(panel);
 
+        //key_listener
+        keyListener();
+
+        setFocusable(true);
+        requestFocus();
+
         this.getContentPane().add(panel);
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
-
     }
 
     private void renderChuKyDienTu(JPanel panel) {
         chuKyDienTuPanel = new JPanel();
-        chuKyDienTuPanel.setPreferredSize(new Dimension(380, 60));
+        chuKyDienTuPanel.setBackground(Color.WHITE);
+        chuKyDienTuPanel.setPreferredSize(new Dimension(480, 80));
         chuKyDienTuPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 
         chuKyDienTuButton = new JButton(Constants.Description.E_SIGNATURE);
-        chuKyDienTuButton.setPreferredSize(new Dimension(130,25));
+        chuKyDienTuButton.setFont(font);
+        chuKyDienTuButton.setFocusable(false);
+        chuKyDienTuButton.setMnemonic(KeyEvent.VK_E);
+        chuKyDienTuButton.setPreferredSize(btnDimension);
         chuKyDienTuPanel.add(chuKyDienTuButton);
         chuKyDienTuButton.setActionCommand(Constants.Description.E_SIGNATURE);
         chuKyDienTuButton.addActionListener(this);
@@ -67,43 +82,73 @@ public class HomeApp extends JFrame implements ActionListener{
 
         panel.add(chuKyDienTuPanel);
     }
+    private void keyListener(){
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                //exit = pressed ESC
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    dispose();
+                }
+            }
+        });
+    }
 
     private void renderMaHoaDoiXung(JPanel panel){
         maHoaDoiXungPanel = new JPanel();
-        maHoaDoiXungPanel.setPreferredSize(new Dimension(380, 90));
-        maHoaDoiXungPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        maHoaDoiXungPanel.setBackground(Color.WHITE);
+        maHoaDoiXungPanel.setPreferredSize(new Dimension(480, 130));
+        maHoaDoiXungPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10,10));
 
         desButton = new JButton(Constants.Cipher.DES);
+        desButton.setMnemonic(KeyEvent.VK_D);
+        desButton.setFont(font);
+        desButton.setFocusable(false);
         desButton.setPreferredSize(btnDimension);
         desButton.setActionCommand(Constants.Cipher.DES);
         desButton.addActionListener(this);
         maHoaDoiXungPanel.add(desButton);
 
         aesButton = new JButton(Constants.Cipher.AES);
+        aesButton.setMnemonic(KeyEvent.VK_A);
+        aesButton.setFont(font);
+        aesButton.setFocusable(false);
         aesButton.setPreferredSize(btnDimension);
         aesButton.setActionCommand(Constants.Cipher.AES);
         aesButton.addActionListener(this);
         maHoaDoiXungPanel.add(aesButton);
 
         tripleDesButton = new JButton(Constants.Cipher.TRIPLE_DES);
+        tripleDesButton.setMnemonic(KeyEvent.VK_S);
+        tripleDesButton.setFont(font);
+        tripleDesButton.setFocusable(false);
         tripleDesButton.setPreferredSize(btnDimension);
         tripleDesButton.setActionCommand(Constants.Cipher.TRIPLE_DES);
         tripleDesButton.addActionListener(this);
         maHoaDoiXungPanel.add(tripleDesButton);
 
         blowfishButton = new JButton(Constants.Cipher.BLOWFISH);
+        blowfishButton.setMnemonic(KeyEvent.VK_B);
+        blowfishButton.setFont(font);
+        blowfishButton.setFocusable(false);
         blowfishButton.setPreferredSize(btnDimension);
         blowfishButton.setActionCommand(Constants.Cipher.BLOWFISH);
         blowfishButton.addActionListener(this);
         maHoaDoiXungPanel.add(blowfishButton);
 
         cast_6Button = new JButton(Constants.Cipher.CAST_6);
+        cast_6Button.setMnemonic(KeyEvent.VK_C);
+        cast_6Button.setFont(font);
+        cast_6Button.setFocusable(false);
         cast_6Button.setPreferredSize(btnDimension);
         cast_6Button.setActionCommand(Constants.Cipher.CAST_6);
         cast_6Button.addActionListener(this);
         maHoaDoiXungPanel.add(cast_6Button);
 
         twofishButton = new JButton(Constants.Cipher.TWOFISH);
+        twofishButton.setMnemonic(KeyEvent.VK_T);
+        twofishButton.setFont(font);
+        twofishButton.setFocusable(false);
         twofishButton.setPreferredSize(btnDimension);
         twofishButton.setActionCommand(Constants.Cipher.TWOFISH);
         twofishButton.addActionListener(this);
@@ -119,10 +164,14 @@ public class HomeApp extends JFrame implements ActionListener{
 
     private void renderMaHoaBatDoiXung(JPanel panel){
         maHoaBatDoiXungPanel = new JPanel();
-        maHoaBatDoiXungPanel.setPreferredSize(new Dimension(380, 60));
+        maHoaBatDoiXungPanel.setBackground(Color.WHITE);
+        maHoaBatDoiXungPanel.setPreferredSize(new Dimension(480, 80));
         maHoaBatDoiXungPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 
         rsaButton = new JButton(Constants.Cipher.RSA);
+        rsaButton.setMnemonic(KeyEvent.VK_R);
+        rsaButton.setFont(font);
+        rsaButton.setFocusable(false);
         rsaButton.setActionCommand(Constants.Cipher.RSA);
         rsaButton.addActionListener(this);
         rsaButton.setPreferredSize(btnDimension);
@@ -138,58 +187,86 @@ public class HomeApp extends JFrame implements ActionListener{
 
     private void renderHashAlgorithmsHash(JPanel panel){
         hashAlgorithmPanel = new JPanel();
-        hashAlgorithmPanel.setPreferredSize(new Dimension(380, 120));
-        hashAlgorithmPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        hashAlgorithmPanel.setBackground(Color.WHITE);
+        hashAlgorithmPanel.setPreferredSize(new Dimension(480, 180));
+        hashAlgorithmPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
 
         md4Button = new JButton(Constants.Cipher.MD4);
+        md4Button.setMnemonic(KeyEvent.VK_4);
+        md4Button.setFont(font);
+        md4Button.setFocusable(false);
         md4Button.setPreferredSize(btnDimension);
         hashAlgorithmPanel.add(md4Button);
         md4Button.setActionCommand(Constants.Cipher.MD4);
         md4Button.addActionListener(this);
 
         md5Button = new JButton(Constants.Cipher.MD5);
+        md5Button.setMnemonic(KeyEvent.VK_5);
+        md5Button.setFont(font);
+        md5Button.setFocusable(false);
         md5Button.setPreferredSize(btnDimension);
         hashAlgorithmPanel.add(md5Button);
         md5Button.setActionCommand(Constants.Cipher.MD5);
         md5Button.addActionListener(this);
 
         sha1Button = new JButton(Constants.Cipher.SHA_1);
+        sha1Button.setMnemonic(KeyEvent.VK_1);
+        sha1Button.setFont(font);
+        sha1Button.setFocusable(false);
         sha1Button.setPreferredSize(btnDimension);
         hashAlgorithmPanel.add(sha1Button);
         sha1Button.setActionCommand(Constants.Cipher.SHA_1);
         sha1Button.addActionListener(this);
 
         sha256Button = new JButton(Constants.Cipher.SHA_256);
+        sha256Button.setMnemonic(KeyEvent.VK_6);
+        sha256Button.setFont(font);
+        sha256Button.setFocusable(false);
         sha256Button.setPreferredSize(btnDimension);
         hashAlgorithmPanel.add(sha256Button);
         sha256Button.setActionCommand(Constants.Cipher.SHA_256);
         sha256Button.addActionListener(this);
 
         sha512Button = new JButton(Constants.Cipher.SHA_512);
+        sha512Button.setMnemonic(KeyEvent.VK_H);
+        sha512Button.setFont(font);
+        sha512Button.setFocusable(false);
         sha512Button.setPreferredSize(btnDimension);
         hashAlgorithmPanel.add(sha512Button);
         sha512Button.setActionCommand(Constants.Cipher.SHA_512);
         sha512Button.addActionListener(this);
 
         sha3_224Button = new JButton(Constants.Cipher.SHA3_224);
+        sha3_224Button.setMnemonic(KeyEvent.VK_2);
+        sha3_224Button.setFont(font);
+        sha3_224Button.setFocusable(false);
         sha3_224Button.setPreferredSize(btnDimension);
         hashAlgorithmPanel.add(sha3_224Button);
         sha3_224Button.setActionCommand(Constants.Cipher.SHA3_224);
         sha3_224Button.addActionListener(this);
 
         crc_32Button = new JButton(Constants.Cipher.CRC_32);
+        crc_32Button.setMnemonic(KeyEvent.VK_3);
+        crc_32Button.setFont(font);
+        crc_32Button.setFocusable(false);
         crc_32Button.setPreferredSize(btnDimension);
         hashAlgorithmPanel.add(crc_32Button);
         crc_32Button.setActionCommand(Constants.Cipher.CRC_32);
         crc_32Button.addActionListener(this);
 
         shake_256Button = new JButton(Constants.Cipher.SHAKE_256);
+        shake_256Button.setMnemonic(KeyEvent.VK_K);
+        shake_256Button.setFont(font);
+        shake_256Button.setFocusable(false);
         shake_256Button.setPreferredSize(btnDimension);
         hashAlgorithmPanel.add(shake_256Button);
         shake_256Button.setActionCommand(Constants.Cipher.SHAKE_256);
         shake_256Button.addActionListener(this);
 
         ripemd_256Button = new JButton(Constants.Cipher.RIPEMD_256);
+        ripemd_256Button.setMnemonic(KeyEvent.VK_P);
+        ripemd_256Button.setFont(font);
+        ripemd_256Button.setFocusable(false);
         ripemd_256Button.setPreferredSize(btnDimension);
         hashAlgorithmPanel.add(ripemd_256Button);
         ripemd_256Button.setActionCommand(Constants.Cipher.RIPEMD_256);
